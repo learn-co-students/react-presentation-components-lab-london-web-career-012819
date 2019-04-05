6600a819963095e07227dc8ee024e1c2587e12b1
Alexander Dubois <sander.dubois@outlook.com>
2019-04-05 13:40:37 +0100

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -2,14 +2,7 @@
 
 import React from 'react'
 
-const handleClick = (event) => {
-    console.log("Sup ladies?")
-    return true
-}
-
-const SimplerComponent = (props) => {
-    return <div onClick={props.handleClick}>I am just happy</div>
-}
+const SimplerComponent = (props) => <div onClick={props.handleClick}>I am just happy</div>
 
 export default SimplerComponent
 
